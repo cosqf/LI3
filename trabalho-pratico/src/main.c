7bcc3afe42d17b604b120374f191fde4bbe8ff19
fcosq <a106837@alunos.uminho.pt>
2024-10-01 23:01:12 +0100

    main e makefile atualizadas ao processInput

--- trabalho-pratico/src/main.c
+++ trabalho-pratico/src/main.c
@@ -1,1 +1,36 @@
-int main () {}
+#include <processInput.h>
+#include <stdlib.h>
+#include <stdio.h>
+#define DEFAULT 500
+
+int main (int argc, char** argv) {
+    if (argc != 3) {
+        perror ("Wrong number of arguments");
+        return 1;
+    }
+    FILE* fp = fopen (argv[1], "r");
+        if (!fp) {
+            perror("Error: File didn't open");
+            return 1;
+        }
+
+    CMD *cmd = malloc (sizeof(CMD));
+    if (cmd == NULL) {
+        perror ("Malloc Error");
+            return 1;
+    }
+
+    char str[DEFAULT];
+    while (fgets (str, sizeof str, fp) != NULL){
+        cmd = getCommand (str, cmd);
+        //if (cmd->querry == 1) querry1 (cmd) 
+    }
+
+    fclose (fp);
+
+    if (cmd) { // fazer função para dar free em tudo?
+        if (cmd->paises) free (cmd->paises);
+        free (cmd);
+    }
+
+}
