cb73873bdd43f1e21df1fe003bca1a465428e76f
a107382 <a107382@alunos.uminho.pt>
2024-10-02 15:08:04 +0100

    add data structures

---
+++ trabalho-pratico/include/musics.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+typedef struct {
+    int hours;     //hh (00-99)
+    int minutes;   //mm (00-59)
+    int seconds;   //ss (00-59)
+} Duration;
+
+typedef struct {
+    int id;              //– identificador único da música;
+    char* title;         //– nome da música;
+    char* artist_id;     //– lista de identificadores dos autores da música;
+    Duration duration;   //– tempo de duração;
+    char* genre;         //– género da música;
+    int year;            //– ano de lançamento;
+    char* lyrics;        //– letra da música.
+} Music;
