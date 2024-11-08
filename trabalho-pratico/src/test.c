43770960ca071cbcb239e2038d64e59591592162
a107382 <a107382@alunos.uminho.pt>
2024-11-08 00:00:21 +0000

    test program path fix and Makefile optimization

--- trabalho-pratico/src/test.c
+++ trabalho-pratico/src/test.c
@@ -4,7 +4,7 @@
 #include <time.h>
 #include <sys/resource.h>
 
-#include <modes.h>
+#include <test.h>
 #include <parsingUtils.h>
 #include <parsing.h>
 #include <utils.h>
@@ -15,41 +15,42 @@
 #include <hashtableManager.h>
 #include <cmdManager.h>
 
-int principal (char* pathData, char* pathCmd) {
-    hashtableManager* mngr = initializeHash ();
+int main (int argc, char** argv){
+    if (argc == 4) tests(argv[1], argv[2], argv[3]); // argv[1]: data, argv[2]: cmd, argv[3]: expected outputs
 
-    getData (pathData, mngr);
-
-    cmdManager* cmdMngr = createCmdManager ();
-    int cmdNumber = readCommands (pathCmd, cmdMngr);
-
-    for (int i = 0; i< cmdNumber; i++) {
-        CMD* cmd = getCommandFromMngr (cmdMngr, i);
-        
-        switch (getCMDquery (cmd)) {
-        case 1:
-            query1 (cmd, getUserManager(mngr), i+1);
-            break;
-
-        case 2:
-            query2 (cmd, mngr, i+1);
-            break;
-
-        case 3:
-            query3 (cmd, getUserManager(mngr), getMusicManager(mngr), i+1);
-            break;
-
-        default:
-            perror ("CMD ERROR");
-            exit (EXIT_FAILURE);
-        }
+    else {
+        perror ("Wrong number of arguments");
+        return 1;
     }
-    freeCmdManager (cmdMngr);
-    freeHash (mngr);
+
     return 0;
 }
 
-void test_principal (char* pathData, char* pathCmd) { // argv[1]: path to data, argv[2]: cmd //flag is 1 if principal is running from the test program, 0 if running alone
+
+int tests (char* pathData, char* pathCmd, char* pathOutput) {
+    struct timespec totalstart, totalend;
+    double totalelapsed;
+    clock_gettime(CLOCK_REALTIME, &totalstart); // Get the start time
+
+    test_principal(pathData, pathCmd, pathOutput);
+
+    // Memory Usage
+    struct rusage r_usage;
+    getrusage(RUSAGE_SELF, &r_usage);
+    long int mem = r_usage.ru_maxrss/1024; // Conversion to MBs
+    printf("Memória usada: %ld MB\n", mem);
+
+    clock_gettime(CLOCK_REALTIME, &totalend); // Get the end time
+
+    totalelapsed = (totalend.tv_sec - totalstart.tv_sec) + (totalend.tv_nsec - totalstart.tv_nsec) / 1e9; // Total elapsed time
+
+    printf("Tempo total de execução: %.2f segundos\n", totalelapsed);
+
+    return 0;
+}
+
+
+void test_principal (char* pathData, char* pathCmd, char* pathOutput) { // argv[1]: path to data, argv[2]: cmd, argv[3]: path to expected outputs
     struct timespec cmdstart, cmdend;
     double q1total = 0, q2total = 0, q3total = 0;
 
@@ -79,7 +80,7 @@ void test_principal (char* pathData, char* pathCmd) { // argv[1]: path to data, 
             q1total += (cmdend.tv_sec - cmdstart.tv_sec) + (cmdend.tv_nsec - cmdstart.tv_nsec) / 1e9;
 
             snprintf(output, sizeof(output), "resultados/command%d_output.txt", i);
-            snprintf(expected, sizeof(expected), "outputs_esperados/command%d_output.txt", i);
+            snprintf(expected, sizeof(expected), "%s/command%d_output.txt", pathOutput, i);
     
             if(compareFiles(output, expected, i) == true) correctQ1++;
 
@@ -95,7 +96,7 @@ void test_principal (char* pathData, char* pathCmd) { // argv[1]: path to data, 
             q2total += (cmdend.tv_sec - cmdstart.tv_sec) + (cmdend.tv_nsec - cmdstart.tv_nsec) / 1e9;
 
             snprintf(output, sizeof(output), "resultados/command%d_output.txt", i);
-            snprintf(expected, sizeof(expected), "outputs_esperados/command%d_output.txt", i);
+            snprintf(expected, sizeof(expected), "%s/command%d_output.txt", pathOutput, i);
     
             if(compareFiles(output, expected, i) == true) correctQ2++;
             break;
@@ -110,7 +111,7 @@ void test_principal (char* pathData, char* pathCmd) { // argv[1]: path to data, 
             q3total += (cmdend.tv_sec - cmdstart.tv_sec) + (cmdend.tv_nsec - cmdstart.tv_nsec) / 1e9;
 
             snprintf(output, sizeof(output), "resultados/command%d_output.txt", i);
-            snprintf(expected, sizeof(expected), "outputs_esperados/command%d_output.txt", i);
+            snprintf(expected, sizeof(expected), "%s/command%d_output.txt", pathOutput, i);
     
             if(compareFiles(output, expected, i) == true) correctQ3++;
             break;
@@ -135,29 +136,6 @@ void test_principal (char* pathData, char* pathCmd) { // argv[1]: path to data, 
 }
 
 
-int tests (char* pathData, char* pathCmd) { // argv[1]: path to data, argv[2]: cmd, argv[3]: expected outputs
-    struct timespec totalstart, totalend;
-    double totalelapsed;
-    clock_gettime(CLOCK_REALTIME, &totalstart); // Get the start time
-
-    test_principal(pathData, pathCmd);
-
-    // Memory Usage
-    struct rusage r_usage;
-    getrusage(RUSAGE_SELF, &r_usage);
-    long int mem = r_usage.ru_maxrss/1024; // Conversion to MBs
-    printf("Memória usada: %ld MB\n", mem);
-
-    clock_gettime(CLOCK_REALTIME, &totalend); // Get the end time
-
-    totalelapsed = (totalend.tv_sec - totalstart.tv_sec) + (totalend.tv_nsec - totalstart.tv_nsec) / 1e9; // Total elapsed time
-
-    printf("Tempo total de execução: %.2f segundos\n", totalelapsed);
-
-    return 0;
-}
-
-
 int compareFiles(char* output, char* expected, int queryn) {
     FILE *outp = fopen(output, "r");
     FILE *exp = fopen(expected, "r");
@@ -178,7 +156,7 @@ int compareFiles(char* output, char* expected, int queryn) {
     // Compare the files line by line
     while (fgets(line1, sizeof(line1), outp) != NULL && fgets(line2, sizeof(line2), exp) != NULL) {
         if (strcmp(line1, line2) != 0) {
-            printf("Descrepância na query %d: linha %d de \"resultados/command%d_output.txt\"\n", queryn, line, queryn);
+            printf("Descrepância na query %d: linha %d de \"%s\"\n", queryn, line, output);
             fclose(outp);
             fclose(exp);
             return 0;
