3a5ba6d5e90bf5705fdc1068691650fd82a86485
andreia <a106915@alunos.uminho.pt>
2024-11-08 10:17:53 +0000

    Adição de ficheiros que escrevem o output

---
+++ trabalho-pratico/include/outputWriter.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void writeQuery1 (FILE* results, char* email, char* firstname, char* lastname, int age, char* country);
+
+void writeQuery2 (FILE* fp, char* name, char* typeString, char* duration, char* artist_country);
+
+void writeQuery3 (FILE* results, char* genre, char* str);
+
+void writeNewLine (FILE* results);
+
+
+
---
+++ trabalho-pratico/src/outputWriter.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void writeQuery1 (FILE* results, char* email, char* firstname, char* lastname, int age, char* country){
+    fprintf(results, "%s;%s;%s;%d;%s", email, firstname, lastname, age, country); //email;first_name;last_name;age;country
+}
+
+void writeQuery2 (FILE* fp, char* name, char* typeString, char* duration, char* artist_country){
+    fprintf (fp, "%s;%s;%s;%s\n", name, typeString, duration, artist_country);
+}
+
+void writeQuery3 (FILE* results, char* genre, char* str){
+    fprintf(results, "%s;%s\n", genre, str);
+}
+
+void writeErrorsArtists (FILE* fp){
+    fprintf (fp, "\"id\";\"name\";\"description\";\"recipe_per_stream\";\"id_constituent\";\"country\";\"type\"\n");
+}
+
+void writeNewLine (FILE* results){
+        fprintf(results, "\n");
+}
